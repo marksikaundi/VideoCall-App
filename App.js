f0899466270fe32c390f5c54c887e35b7852e8b2
marksikaundi <emmanuelmark117@gmail.com>
2022-01-18 17:03:58 +0200

    frontend process

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, FlatList } from 'react-native';
+import contacts from './assets/data/contacts.json';
 
-export default function App() {
+// const contacts = ['Mark, Elite, Jeff'];
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <StatusBar barStyle={'dark-content'} />
+      <View style={styles.page}>
+      <FlatList 
+      data={contacts} renderItem={({item}) => (
+      <Text style={styles.contactName}>{item.user_display_name}</Text>
+      )} 
+      ItemSeparatorComponent={() => <View style={styles.separator} />}
+      />
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  page: {
+    padding: 15,
+  },
+  contactName: {
+    fontSize: 18,
+    marginVertical: 5,
+  },
+  separator: {
+    width: '100%',
+    height: 1,
+    backgroundColor: '#bfbfbf',
   },
 });
+
+export default App;
